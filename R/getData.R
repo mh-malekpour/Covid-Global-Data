fdd97e07dc017f92a3d30ca0643ea6f4bcb731d2
mhmp98 <mohammadhossein.malekpour@gmail.com>
2020-10-26 11:40:55 +0330

    edit dataset

--- R/getData.R
+++ R/getData.R
@@ -1,44 +1,49 @@
 getData <- function(){
 
+  library(dplyr)
+
   confirmedURL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
   deathsURL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
   recoveredURL <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
   confirmed <- as_tibble(read_csv(confirmedURL, col_types = cols()) %>%
-                           select(-'Province/State', -'Lat', -'Long') %>%
                            rename("country" = "Country/Region")%>%
                            mutate(country = as.factor(country)))
 
-  recoverd <- as_tibble(read_csv(confirmedURL, col_types = cols()) %>%
-                          select(-'Province/State', -'Lat', -'Long') %>%
+  recoverd <- as_tibble(read_csv(recoveredURL, col_types = cols()) %>%
                           rename("country" = "Country/Region")%>%
                           mutate(country = as.factor(country)))
 
-  deaths <- as_tibble(read_csv(confirmedURL, col_types = cols()) %>%
-                        select(-'Province/State', -'Lat', -'Long') %>%
+  deaths <- as_tibble(read_csv(deathsURL, col_types = cols()) %>%
                         rename("country" = "Country/Region")%>%
                         mutate(country = as.factor(country)))
 
   confirmed %>%
-    gather(key = "date", value = "count", 2:ncol(confirmed)) %>%
+    gather(key = "date", value = "count", 5:ncol(confirmed)) %>%
     mutate(lable = "confirmed",
            count = c(count[1] , diff(count)),
            date=as.Date(date, format = "%m/%d/%y")) %>%
-    select(country, date, lable, count) -> confirmed
+    select(country, date, lable, count) %>%
+    group_by(country, date, lable) %>%
+    summarise(count = sum(count)) -> confirmed
 
   recoverd %>%
-    gather(key = "date", value = "count", 2:ncol(confirmed)) %>%
+    gather(key = "date", value = "count", 5:ncol(recoverd)) %>%
     mutate(lable = "recoverd" ,
            count = c(count[1] , diff(count)),
            date=as.Date(date, format = "%m/%d/%y")) %>%
-    select(country, date, lable, count) -> recoverd
+    select(country, date, lable, count) %>%
+    group_by(country, date, lable) %>%
+    summarise(count = sum(count)) -> recoverd
 
   deaths %>%
-    gather(key = "date", value = "count", 2:ncol(confirmed)) %>%
+    gather(key = "date", value = "count", 5:ncol(deaths)) %>%
     mutate(lable = "death",
            count = c(count[1] , diff(count)),
            date=as.Date(date, format = "%m/%d/%y")) %>%
-    select(country, date, lable, count) -> deaths
+    select(country, date, lable, count) %>%
+    group_by(country, date, lable) %>%
+    summarise(count = sum(count))  -> deaths
 
   covidData <- bind_rows(confirmed, recoverd, deaths) %>%
     mutate(date=as.Date(date, format = "%m/%d/%y"))
